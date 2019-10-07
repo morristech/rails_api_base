e0dd187c7482781580c4936bc01e4bb0f273afc8
Mario Souto <mariosouto@outlook.com>
2019-10-07 15:16:55 -0300

    Fix SimpleCov start

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,16 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov'
+SimpleCov.start 'rails'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/core'
 require 'spec_helper'
 require 'rspec/rails'
-require 'simplecov'
-
-SimpleCov.start do
-  add_filter 'spec/'
-end
 
 ActiveRecord::Migration.maintain_test_schema!
 WebMock.disable_net_connect!(allow_localhost: true)
